9d91a009cc1311e6e21c592cb18fcad58d4c0c7a
huoxj <2567499168@qq.com>
2024-08-16 11:42:16 +0800

    initial commit

---
+++ frontend/src/components/menu-item.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const props = defineProps(['name'])
+const url = ref("https://cdn.uviewui.com/uview/album/".concat(Math.floor(Math.random() * 10 + 1).toString()).concat(".jpg"))
+
+
+</script>
+
+<template>
+  <u-card
+    :border="false"
+    :head-border-bottom="false"
+    padding="0"
+    margin="10rpx 5rpx 5rpx 5rpx">
+    <template #body>
+      <image
+        :src=url
+        class="image"
+        mode="widthFix" :lazy-load="true">
+      </image>
+      <u-text
+        class="title"
+        :text="name"
+        :bold="true"
+        size="33rpx"
+        margin="0rpx 0rpx 10rpx 20rpx"
+        wordWrap="anywhere"
+        style="width: 95%"></u-text>
+    </template>
+  </u-card>
+</template>
+
+<style scoped>
+.image{
+  width: 100%;
+}
+</style>
---
+++ frontend/src/components/tabbar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <u-tabbar>
+    <u-tabbar-item icon="home" text="首页" to="/index"></u-tabbar-item>
+    <u-tabbar-item icon="list" text="食堂" to="/category"></u-tabbar-item>
+    <u-tabbar-item icon="search" text="菜根一下" to="/search"></u-tabbar-item>
+    <u-tabbar-item icon="grid" text="分类" to="/cart"></u-tabbar-item>
+    <u-tabbar-item icon="account" text="我的" to="/user"></u-tabbar-item>
+  </u-tabbar>
+</template>
+
+<style scoped>
+
+</style>
